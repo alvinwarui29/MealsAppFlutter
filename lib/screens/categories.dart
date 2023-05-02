d543cb51987dde3d692a16370a4f7aa7db045445
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-02 11:18:26 +0300

    send user to meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/screens/meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(title: 'Some data', meal: [])));
+  }
 
   @override
   Widget build(BuildContext context) {
