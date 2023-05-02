8b1d9113b8055bffd356666b9875b93bbbc258ea
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-02 16:41:23 +0300

    finish meal detail

--- lib/screens/meals_detail.dart
+++ lib/screens/meals_detail.dart
@@ -9,20 +9,39 @@ class MealsDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(meal.title)),
-        body: Column(
+        body: ListView(
+          padding: const EdgeInsets.only(bottom: 30),
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-            ),
-            Text('Ingredients',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground)),
-            for (final ingredients in meal.ingredients)
-              Text(ingredients,
-                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground)),
+            Column(
+              children: [
+                Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                ),
+                Text('Ingredients',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground)),
+                const SizedBox(
+                  height: 14,
+                ),
+                for (final ingredients in meal.ingredients)
+                  Text(ingredients,
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground)),
+                const SizedBox(
+                  height: 14,
+                ),
+                Text('Steps',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground)),
+                for (final steps in meal.steps)
+                  Text(steps,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground)),
+              ],
+            )
           ],
         ));
   }
