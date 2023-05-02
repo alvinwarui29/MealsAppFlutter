ad59bbac98e3819245775ea70516827d2bd649b1
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-02 10:34:41 +0300

    create a constructor for the meals page

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:meals/model/meal.dart';
 
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meal});
+  final String title;
+  final List<Meal> meal;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+    );
+  }
+}
