4122f62df8e60988b1adb4102d1f51ef9c42bf38
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-02 10:50:43 +0300

    load meal data in meals screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -7,8 +7,38 @@ class MealsScreen extends StatelessWidget {
   final List<Meal> meal;
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemBuilder: (context, index) => Text(meal[index].title),
+    );
+    if (meal.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'oh...uh Nothing here',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Try out another category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(title: Text(title)),
+      body: content,
     );
   }
 }
